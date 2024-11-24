b8ec84fff6e15e9d88cf31fca2b3fa1f7f14e796
Anurag-Kalwa <anurag852001@gmail.com>
2024-11-24 21:53:59 +0530

    first api

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,35 @@
-fn main() {
-    print!("hello love");
+use actix_web::middleware::Logger;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use serde_json::json;
+use log::info;
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    for  i in 1..=10 {
+        print!("{}", i);
+    }
+    HttpResponse::Ok().json(json!({
+        "message": format!("Hello, {}!", name)
+    }))
 }
+
+    #[actix_web::main] // or #[tokio::main]
+    async fn main() -> std::io::Result<()> {
+        for  i in 1..=10 {
+            println!("{}", i);
+        }
+        if std::env::var_os("RUST_LOG").is_none(){
+            std::env::set_var("RUST_LOG", "actix_web=info,actix_server=info");
+        }
+        dotenv::dotenv().ok();
+        env_logger::init();
+        HttpServer::new(|| {
+            App::new().wrap(Logger::default()).service(greet)
+
+        })
+            .bind(("127.0.0.1", 9000))?
+            .run()
+            .await
+    }
+
 
