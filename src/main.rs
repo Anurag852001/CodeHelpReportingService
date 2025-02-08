f6cca4e7c136ece355f7a7e6d3a9d030fadcc918
Anurag <anurag852001@gmail.com>
2025-02-08 16:49:04 +0530

    Added cors handler

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod r#struct;
 mod mongo;
 mod r#enum;
 
+use actix_cors::Cors;
 use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer, Responder};
 
@@ -21,7 +22,8 @@ use actix_web::{App, HttpServer, Responder};
         dotenv::dotenv().ok();
         env_logger::init();
         HttpServer::new(|| {
-            App::new().wrap(Logger::default()).configure(routes::code_help_reporting_routes::config)
+            App::new().wrap(Cors::default().allow_any_header().allow_any_method().allow_any_origin() ).wrap(Logger::default()).configure(routes::code_help_reporting_routes::config)
+
 
         })
             .bind((address, port))?
